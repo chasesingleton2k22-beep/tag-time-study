88b33daeac16fdeb90f445ec9b3008a4f26a1152
Chase Singleton <CSingleton@tagmfg.us>
2026-08-04 10:59:07 -0400

    Undo Weld/Fabrication exclusivity; add Positioning Jig and Cleaning Weld
    Weld and Fabrication now both show on every process again, regardless
    of process name. Also adds two new elements: Positioning Jig (amber,
    distinct from Placing Parts in Jig) and Cleaning Weld (red, distinct
    from Cleaning Welding Lead).

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tag-time-study-v81';
+const CACHE_NAME = 'tag-time-study-v82';
 
 const CORE_ASSETS = ['./', './index.html', './logo.png'];
 const CDN_SHEETJS = 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js';
